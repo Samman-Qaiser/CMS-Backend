36b3d41647781b4531dc268563596e2436e70406
Samman Qaiser <sammansheikh6@gmail.com>
2026-05-19 10:09:49 +0500

    fix cloudinary export

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 import {User} from '../models/User.js'
 import cloudinary from '../config/cloudinary.js'
 import bcrypt from 'bcryptjs'
-
+import { cloudinary } from '../config/cloudinary.js'
 
 
 // ─── Helper: Cloudinary Upload ───────────────────────
